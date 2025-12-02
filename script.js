e469c19697f8ffeb1e9ab5f0b5b2ef29636b78a9
Jack-Art-Lab-OS <1jack.sparrow2.1687@gmail.com>
2025-12-02 20:19:45 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showAlert() {
+  const modal = document.getElementById("customAlertModal");
+  modal.style.display = "flex"; // Use flex to center easily
+}
+
+function closeAlert() {
+  const modal = document.getElementById("customAlertModal");
+  modal.style.display = "hide";
+}
